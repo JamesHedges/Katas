dff07c10e69440543d6d5e8ac4c3f1abe0c3d5f1
James Hedges <JamesHedges@comcast.net>
2017-10-10 18:52:30 -0600

    Adding simple OO solution using polymorphism. Added basic Purchase Order creation.

---
+++ FunBooksAndVideos/SimpleOOwithPolymorphism/Src/Order.Tests/PurchaseOrderTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Shouldly;
+using Order;
+
+namespace Order.Tests
+{
+    public class PurchaseOrderTests
+    {
+        [Fact]
+        public void CreatePurchaseOrder()
+        {
+            decimal total = 48.5m;
+            int customerId = 4567890;
+            List<ItemLine> itemLines = new List<ItemLine>
+            {
+                new ItemLine { Description = "Comprehensive First Aid Training", Type = ItemLineType.Video },
+                new ItemLine { Description = "The Girl on the Train", Type = ItemLineType.Book },
+                new ItemLine { Description = "Book Club Membership", Type = ItemLineType.BookClub },
+            };
+
+            PurchaseOrder po = PurchaseOrder.Create(total, customerId, itemLines);
+
+            po.ShouldNotBeNull();
+            po.Id.ShouldBeGreaterThan(0);
+            po.CustomerId.ShouldBe(customerId);
+            po.Total.ShouldBe(total);
+            po.ItemLines.Count().ShouldBe(itemLines.Count());
+        }
+    }
+}
---
+++ FunBooksAndVideos/SimpleOOwithPolymorphism/Src/Order/PurchaseOrder.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Order
+{
+    public class PurchaseOrder
+    {
+        private static int _LastId;
+
+        static PurchaseOrder()
+        {
+            Random random = new Random();
+            _LastId = random.Next(0, 4999999);
+        }
+
+        private PurchaseOrder()
+        {
+            Id = NextId();
+        }
+
+        public int Id { get; }
+        public decimal Total { get; private set; }
+        public int CustomerId { get; private set; }
+        public IEnumerable<ItemLine> ItemLines { get; private set; }
+
+        public static PurchaseOrder Create(decimal total, int customerId, IEnumerable<ItemLine> itemLines)
+        {
+            PurchaseOrder po = new PurchaseOrder
+            {
+                Total = total,
+                CustomerId = customerId,
+                ItemLines = itemLines
+            };
+
+            return po;
+        }
+
+        private static int NextId()
+        {
+            return ++_LastId;
+        }
+    }
+
+    public class ItemLine
+    {
+        public string Description { get; set; }
+        public ItemLineType Type { get; set; }
+    }
+
+    public enum ItemLineType
+    {
+        Video,
+        Book,
+        BookClub
+    }
+}
